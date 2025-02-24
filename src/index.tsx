f14575d47da04aee8e47804a7c8ec50c7fea5b88
Mustafa Shikora <mshik3@gmail.com>
2025-02-24 00:05:32 -0600

    fixing router

--- src/index.tsx
+++ src/index.tsx
@@ -15,12 +15,8 @@ document.head.appendChild(link);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/tirth-olympics">
+    <BrowserRouter basename="">
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
